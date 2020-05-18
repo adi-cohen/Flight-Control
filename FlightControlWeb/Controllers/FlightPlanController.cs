37bda569addb9acd75c3cca7052299fc12becced
adi cohen <Adidc10@gmail.com>
2020-05-18 12:25:47 +0300

    create new DB , add FlightPlan and Segments

---
+++ FlightControlWeb/Controllers/FlightPlanController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FlightControlWeb.Models;
+
+namespace FlightControlWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FlightPlanController : ControllerBase
+    {
+        private readonly DBInteractor db;
+
+        public FlightPlanController(DBInteractor newDb)
+        {
+            db = newDb;
+        }
+
+        // GET: api/FlightPlan
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<FlightPlan>>> GetFlightPlans()
+        {
+            return await db.FlightPlans.ToListAsync();
+           
+            //return await db.Segments.ToListAsync();
+        }
+
+        // GET: api/FlightPlan/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<FlightPlan>> GetFlightPlan(long id)
+        {
+            var flightPlan = await db.FlightPlans.FindAsync(id);
+
+            if (flightPlan == null)
+            {
+                return NotFound();
+            }
+
+            return flightPlan;
+        }
+
+        // PUT: api/FlightPlan/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutFlightPlan(long id, FlightPlan flightPlan)
+        {
+            if (id != flightPlan.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(flightPlan).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FlightPlanExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/FlightPlan
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<FlightPlan>> PostFlightPlan(FlightPlan flightPlan)
+        {
+            db.FlightPlans.Add(flightPlan);
+            await db.SaveChangesAsync();
+
+            return CreatedAtAction("GetFlightPlan", new { id = flightPlan.Id }, flightPlan);
+        }
+
+        // DELETE: api/FlightPlan/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<FlightPlan>> DeleteFlightPlan(long id)
+        {
+            var flightPlan = await db.FlightPlans.FindAsync(id);
+            if (flightPlan == null)
+            {
+                return NotFound();
+            }
+
+            db.FlightPlans.Remove(flightPlan);
+            await db.SaveChangesAsync();
+            return flightPlan;
+        }
+
+        private bool FlightPlanExists(long id)
+        {
+            return db.FlightPlans.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ FlightControlWeb/Models/DBInteractor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using FlightControlWeb;
+    
+namespace FlightControlWeb.Models
+{
+    public class DBInteractor : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+            .UseSqlite(@"Data Source = FlightControl.db;");
+        }
+        public DbSet<FlightPlan> FlightPlans { get; set; }
+        public DbSet<Segment> Segments { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<FlightPlan>().HasData(
+            new FlightPlan() { Id = 1, Passengers = 4 , CompanyName = "combo", StartLongitude = 31.22, StartLatitude = 32.44, StartDate = DateTime.Now, Segments =null},
+            new FlightPlan() { Id = 2, Passengers = 3, CompanyName = "mmba", StartLongitude = 31.44, StartLatitude = 32.33, StartDate = DateTime.Now, Segments = null}
+            );
+        }
+    }
+}
+
