eb6d102770d14a545c225ff0a3a113b0ae5817dd
Boris Ilchenkov <boris.ilchenkov@gmail.com>
2020-05-20 11:36:14 +0300

    Added ServerManager class

---
+++ FlightControlWeb/Models/ServerManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace FlightControlWeb.Models
+{
+    public class ServerManager
+    {
+        public static async Task<dynamic> makeExternalRequest(string uri)
+        {
+            var client = new HttpClient();
+            var result = await client.GetStringAsync(uri);
+            dynamic json = JsonConvert.DeserializeObject(result);
+            return json;
+        }
+    }
+}
