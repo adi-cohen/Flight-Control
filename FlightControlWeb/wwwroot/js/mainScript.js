ed8d070800dfa2395a250af430c288e27d16291f
Omer Mokady <omer.mokady@live.biu.ac.il>
2020-05-21 15:13:25 +0300

    making "showFlightDetails" function

--- FlightControlWeb/wwwroot/js/mainScript.js
+++ FlightControlWeb/wwwroot/js/mainScript.js
@@ -1,4 +1,7 @@
-﻿func = function dataUpdate() {
+﻿let mymap = L.map('mapId').setView([51.505, -0.09], 1);
+createMap();
+
+func = function dataUpdate() {
     let getOptions = prepareGetAll();
     let date = getDate();
     //let url = "/api/Flights?relative_to=".concat(date).concat("&sync_all");
@@ -18,10 +21,51 @@
                     addToMyFlight(response[i]);
                     //console.log(response[i]);
                 }
+                addToMap(response[i]);
+
             }
         }
     });
     
+}
+
+function createMap() {
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoib21lcm1vayIsImEiOiJja2FnaXlvbmMwNDFpMnhtb2ptdnJ3NnZhIn0._fxKcKn0NhvxL8chgMNzYQ', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        minZoom: 1,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: 'pk.eyJ1Ijoib21lcm1vayIsImEiOiJja2FnaXlvbmMwNDFpMnhtb2ptdnJ3NnZhIn0._fxKcKn0NhvxL8chgMNzYQ'
+    }).addTo(mymap);
+}
+
+
+
+function addToMap(flight) {
+    let blackIcon = L.icon({
+        iconUrl: 'blackAirplane.png',
+        iconSize: [38, 40], // size of the icon
+        iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+        popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+    });
+    //blackIcon.className = flight.id;
+    let marker1 = L.marker([flight.startLongitude, flight.startLatitude], { icon: blackIcon }).addTo(mymap);
+    marker1.className = flight.id;
+    marker1.on('click', showFlightDetails);
+
+
+
+
+}
+
+function showFlightDetails(event) {
+    console.log(this.className);
+    // get for json for certain id
+    let path = [[34, 42], [40, 44]];
+    var polyline = L.polyline(path, { color: 'red' }).addTo(mymap);
+    mymap.fitBounds(polyline.getBounds());
 }
 
 function getDate() {
@@ -69,11 +113,12 @@ function addToMyFlight(flight) {
     icon.innerText = "delete";
 
     //column 1
-    myFlightsElRow.id = flight.id;
+    myFlightElCl1.id = flight.id;
+    myFlightElCl1.className = flight.id;
     let x = flight.id.toString() + flight.company_name; //unique name of flight
     let text = document.createTextNode(x);
     myFlightElCl1.appendChild(text);
-    myFlightElCl1.onclick = testGetDetails; //event handler
+    myFlightElCl1.onclick = showFlightDetails; //event handler
 
     //column 2
     myFlightElCl2.appendChild(icon);
